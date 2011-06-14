985e3f10db5035041c86724b3bb05e6295038583
Thomas <Thomas@Thomas-PC>
2011-06-14 17:40:32 +0100

    More TABS -> Spaces

--- src/com/Top_Cat/CODMW/listeners/CODBlockListener.java
+++ src/com/Top_Cat/CODMW/listeners/CODBlockListener.java
@@ -15,71 +15,66 @@ import com.Top_Cat.CODMW.objects.claymore;
 import com.Top_Cat.CODMW.objects.sentry;
 
 public class CODBlockListener extends BlockListener {
-	
-	main plugin;
-	
-	public CODBlockListener(main instance) {
-		plugin = instance;
-	}
-	
-	@Override
-	public void onBlockDamage(BlockDamageEvent event) {
-		if (!event.getPlayer().isOp()) {
-			event.setCancelled(true);
-		}
-	}
-	
-	public int rotateblock(Player p, Block b) {
-		int yaw = (int) p.getLocation().getYaw();
-		if (yaw < -45) { yaw += 360; }
-		if (yaw >= 315) { yaw -= 359; }
-		int r = (int) Math.ceil((yaw + 45) / 90);
-		switch (r) {
-			case 1: b.setData((byte) 4); break;
-			case 2: b.setData((byte) 2); break;
-			case 3: b.setData((byte) 5); break;
-			case 4: b.setData((byte) 3); break;
-		}
-		return r;
-	}
-	
-	@Override
-	public void onBlockPlace(BlockPlaceEvent event) {
-		if (event.getBlockPlaced().getType() == Material.WALL_SIGN) {
-			int r = rotateblock(event.getPlayer(), event.getBlockPlaced());
-			Sign s = (Sign) event.getBlockPlaced().getState();
-			if (plugin.players.get(event.getPlayer()).getTeam() == team.DIAMOND) {
-				s.setLine(0, plugin.d + "b** DIAMOND **");
-				s.setLine(3, plugin.d + "b** DIAMOND **");
-			} else {
-				s.setLine(0, plugin.d + "6-- GOLD --");
-				s.setLine(3, plugin.d + "6-- GOLD --");
-			}
-			
-			s.setLine(1, "This side");
-			s.setLine(2, "towards enemy");
-			s.update();
-			plugin.clays.add(new claymore(plugin, event.getBlockPlaced(), r, event.getPlayer()));
-		} else if (event.getBlockPlaced().getType() == Material.DISPENSER && event.getBlockPlaced().getRelative(0, 1, 0).getType() == Material.AIR) {
-			event.getBlockPlaced().setType(Material.FENCE);
-			event.getBlockPlaced().getRelative(0, 1, 0).setType(Material.DISPENSER);
-			int r = rotateblock(event.getPlayer(), event.getBlockPlaced().getRelative(0, 1, 0));
-			new sentry(plugin, event.getBlockPlaced(), r, event.getPlayer());
-		} else if (!event.getPlayer().isOp()) {
-			event.setBuild(false);
-		}
-	}
+    
+    main plugin;
+    
+    public CODBlockListener(main instance) {
+        plugin = instance;
+    }
+    
+    @Override
+    public void onBlockDamage(BlockDamageEvent event) {
+        if (!event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
+    }
+    
+    public int rotateblock(Player p, Block b) {
+        int yaw = (int) p.getLocation().getYaw();
+        if (yaw < -45) { yaw += 360; }
+        if (yaw >= 315) { yaw -= 359; }
+        int r = (int) Math.ceil((yaw + 45) / 90);
+        switch (r) {
+            case 1: b.setData((byte) 4); break;
+            case 2: b.setData((byte) 2); break;
+            case 3: b.setData((byte) 5); break;
+            case 4: b.setData((byte) 3); break;
+        }
+        return r;
+    }
+    
+    @Override
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (event.getBlockPlaced().getType() == Material.WALL_SIGN) {
+            int r = rotateblock(event.getPlayer(), event.getBlockPlaced());
+            Sign s = (Sign) event.getBlockPlaced().getState();
+            if (plugin.players.get(event.getPlayer()).getTeam() == team.DIAMOND) {
+                s.setLine(0, plugin.d + "b** DIAMOND **");
+                s.setLine(3, plugin.d + "b** DIAMOND **");
+            } else {
+                s.setLine(0, plugin.d + "6-- GOLD --");
+                s.setLine(3, plugin.d + "6-- GOLD --");
+            }
+            
+            s.setLine(1, "This side");
+            s.setLine(2, "towards enemy");
+            s.update();
+            plugin.clays.add(new claymore(plugin, event.getBlockPlaced(), r, event.getPlayer()));
+        } else if (event.getBlockPlaced().getType() == Material.DISPENSER && event.getBlockPlaced().getRelative(0, 1, 0).getType() == Material.AIR) {
+            event.getBlockPlaced().setType(Material.FENCE);
+            event.getBlockPlaced().getRelative(0, 1, 0).setType(Material.DISPENSER);
+            int r = rotateblock(event.getPlayer(), event.getBlockPlaced().getRelative(0, 1, 0));
+            new sentry(plugin, event.getBlockPlaced(), r, event.getPlayer());
+        } else if (!event.getPlayer().isOp()) {
+            event.setBuild(false);
+        }
+    }
 
-	@Override
-	public void onBlockBreak(BlockBreakEvent event) {
-		if (!event.getPlayer().isOp()) {
-			event.setCancelled(true);
-		}
-	}
-	
-	//@Override
-	//public void onBlockCanBuild(BlockCanBuildEvent event) {
-		//event.setBuildable(false);
-	//}
-	
+    @Override
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (!event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
+    }
+    
 }
--- src/com/Top_Cat/CODMW/listeners/CODEntityListener.java
+++ src/com/Top_Cat/CODMW/listeners/CODEntityListener.java
@@ -20,96 +20,96 @@ import com.Top_Cat.CODMW.main;
 import com.Top_Cat.CODMW.objects.CArrow;
 
 public class CODEntityListener extends EntityListener {
-	
-	private final main plugin;
-	
-	public CODEntityListener(main instance) {
+    
+    private final main plugin;
+    
+    public CODEntityListener(main instance) {
         plugin = instance;
     }
-	
-	@Override
-	public void onPaintingBreak(PaintingBreakEvent event) {
-		if (event.getCause() == RemoveCause.WORLD || !(((PaintingBreakByEntityEvent) event).getRemover() instanceof Player) || !(((Player) ((PaintingBreakByEntityEvent) event).getRemover()).isOp())) {
-			event.setCancelled(true);
-		}
-	}
-	
-	@Override
-	public void onPaintingPlace(PaintingPlaceEvent event) {
-		if (!event.getPlayer().isOp()) {
-			event.setCancelled(true);
-		}
-	}
-	
-	@Override
-	public void onEntityDamage(EntityDamageEvent event) {
-		event.setCancelled(true);
-		try {
-			if (event.getCause() == DamageCause.FALL && event.getDamage() >= 4) {
-				if (event.getEntity() instanceof Player) {
-					plugin.players.get((Player) event.getEntity()).incHealth(1, (Player) event.getEntity(), 0);
-				}
-				event.setCancelled(false);
-			}
-			event.setDamage(1);
-			if (event instanceof EntityDamageByProjectileEvent) {
-				if (event.getEntity() instanceof Wolf) {
-					plugin.wolves.remove(event.getEntity());
-					event.getEntity().remove();
-					return;
-				} else {
-					int reason = 2;
-					if (((CraftArrow) ((EntityDamageByProjectileEvent) event).getProjectile()).getHandle() instanceof CArrow) {
-						reason = ((CArrow) ((CraftArrow) ((EntityDamageByProjectileEvent) event).getProjectile()).getHandle()).reason;
-					}
-					Player attacker = (Player) (((EntityDamageByProjectileEvent) event).getDamager());
-					Player defender = (Player) (((EntityDamageByProjectileEvent) event).getEntity());
-					if (plugin.players.get(attacker).getTeam() != plugin.players.get(defender).getTeam()) {
-						plugin.players.get(defender).incHealth(1, attacker, reason);
-						event.setCancelled(false);
-					}
-				}
-			} else if (event instanceof EntityDamageByEntityEvent) {
-				if (((EntityDamageByEntityEvent) event).getDamager() instanceof Player && event.getEntity() instanceof Player) {
-					Player attacker = (Player) (((EntityDamageByEntityEvent) event).getDamager());
-					Player defender = (Player) (((EntityDamageByEntityEvent) event).getEntity());
-					Location a = attacker.getLocation();
-					Location d = defender.getLocation();
-					if (attacker.getItemInHand().getType() == Material.IRON_SWORD) {
-						if (plugin.players.get(attacker).getTeam() != plugin.players.get(defender).getTeam()) {
-							double dist = Math.sqrt(Math.pow(a.getX() - d.getX(), 2) + Math.pow(a.getY() - d.getY(), 2) + Math.pow(a.getZ() - d.getZ(), 2));
-							if (dist < 1.5) {
-								plugin.players.get(defender).incHealth(2, attacker, 1);
-								event.setCancelled(false);
-							}
-						}
-						if (defender.getHealth() < 3) {
-							event.setDamage(0);
-						}
-					}
-				} else if (((EntityDamageByEntityEvent) event).getDamager() instanceof Wolf && ((EntityDamageByEntityEvent) event).getEntity() instanceof Player) {
-					Player defender = (Player) (((EntityDamageByEntityEvent) event).getEntity());
-					plugin.wolves.remove(((EntityDamageByEntityEvent) event).getDamager());
-					plugin.players.get(defender).incHealth(2, (Player) ((Wolf) ((EntityDamageByEntityEvent) event).getDamager()).getOwner(), 4);
-					((Wolf) ((EntityDamageByEntityEvent) event).getDamager()).remove();
-				} else if (((EntityDamageByEntityEvent) event).getEntity() instanceof Wolf) {
-					plugin.wolves.remove(event.getEntity());
-					((EntityDamageByEntityEvent) event).getEntity().remove();
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (event.getEntity() instanceof Player) {
-				((Player) event.getEntity()).teleport(plugin.teamselect);
-			}
-		}
-	}
-	
-	@Override
-	public void onCreatureSpawn(CreatureSpawnEvent event) {
-		if (!(event.getEntity() instanceof Wolf)) {
-			event.setCancelled(true);
-		}
-	}
-	
+    
+    @Override
+    public void onPaintingBreak(PaintingBreakEvent event) {
+        if (event.getCause() == RemoveCause.WORLD || !(((PaintingBreakByEntityEvent) event).getRemover() instanceof Player) || !(((Player) ((PaintingBreakByEntityEvent) event).getRemover()).isOp())) {
+            event.setCancelled(true);
+        }
+    }
+    
+    @Override
+    public void onPaintingPlace(PaintingPlaceEvent event) {
+        if (!event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
+    }
+    
+    @Override
+    public void onEntityDamage(EntityDamageEvent event) {
+        event.setCancelled(true);
+        try {
+            if (event.getCause() == DamageCause.FALL && event.getDamage() >= 4) {
+                if (event.getEntity() instanceof Player) {
+                    plugin.players.get((Player) event.getEntity()).incHealth(1, (Player) event.getEntity(), 0);
+                }
+                event.setCancelled(false);
+            }
+            event.setDamage(1);
+            if (event instanceof EntityDamageByProjectileEvent) {
+                if (event.getEntity() instanceof Wolf) {
+                    plugin.wolves.remove(event.getEntity());
+                    event.getEntity().remove();
+                    return;
+                } else {
+                    int reason = 2;
+                    if (((CraftArrow) ((EntityDamageByProjectileEvent) event).getProjectile()).getHandle() instanceof CArrow) {
+                        reason = ((CArrow) ((CraftArrow) ((EntityDamageByProjectileEvent) event).getProjectile()).getHandle()).reason;
+                    }
+                    Player attacker = (Player) (((EntityDamageByProjectileEvent) event).getDamager());
+                    Player defender = (Player) (((EntityDamageByProjectileEvent) event).getEntity());
+                    if (plugin.players.get(attacker).getTeam() != plugin.players.get(defender).getTeam()) {
+                        plugin.players.get(defender).incHealth(1, attacker, reason);
+                        event.setCancelled(false);
+                    }
+                }
+            } else if (event instanceof EntityDamageByEntityEvent) {
+                if (((EntityDamageByEntityEvent) event).getDamager() instanceof Player && event.getEntity() instanceof Player) {
+                    Player attacker = (Player) (((EntityDamageByEntityEvent) event).getDamager());
+                    Player defender = (Player) (((EntityDamageByEntityEvent) event).getEntity());
+                    Location a = attacker.getLocation();
+                    Location d = defender.getLocation();
+                    if (attacker.getItemInHand().getType() == Material.IRON_SWORD) {
+                        if (plugin.players.get(attacker).getTeam() != plugin.players.get(defender).getTeam()) {
+                            double dist = Math.sqrt(Math.pow(a.getX() - d.getX(), 2) + Math.pow(a.getY() - d.getY(), 2) + Math.pow(a.getZ() - d.getZ(), 2));
+                            if (dist < 1.5) {
+                                plugin.players.get(defender).incHealth(2, attacker, 1);
+                                event.setCancelled(false);
+                            }
+                        }
+                        if (defender.getHealth() < 3) {
+                            event.setDamage(0);
+                        }
+                    }
+                } else if (((EntityDamageByEntityEvent) event).getDamager() instanceof Wolf && ((EntityDamageByEntityEvent) event).getEntity() instanceof Player) {
+                    Player defender = (Player) (((EntityDamageByEntityEvent) event).getEntity());
+                    plugin.wolves.remove(((EntityDamageByEntityEvent) event).getDamager());
+                    plugin.players.get(defender).incHealth(2, (Player) ((Wolf) ((EntityDamageByEntityEvent) event).getDamager()).getOwner(), 4);
+                    ((Wolf) ((EntityDamageByEntityEvent) event).getDamager()).remove();
+                } else if (((EntityDamageByEntityEvent) event).getEntity() instanceof Wolf) {
+                    plugin.wolves.remove(event.getEntity());
+                    ((EntityDamageByEntityEvent) event).getEntity().remove();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (event.getEntity() instanceof Player) {
+                ((Player) event.getEntity()).teleport(plugin.teamselect);
+            }
+        }
+    }
+    
+    @Override
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (!(event.getEntity() instanceof Wolf)) {
+            event.setCancelled(true);
+        }
+    }
+    
 }
