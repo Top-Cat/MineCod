dc4a940fc73003f41d68b5a8eca571431ad82ac0
Thomas Cheyney <tom@thegigcast.net>
2011-07-24 14:30:36 +0100

    Unneeded imports

--- src/com/Top_Cat/CODMW/objects/grenade.java
+++ src/com/Top_Cat/CODMW/objects/grenade.java
@@ -3,7 +3,6 @@ package com.Top_Cat.CODMW.objects;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Type;
 import org.bukkit.inventory.ItemStack;
 
 import com.Top_Cat.CODMW.main;
